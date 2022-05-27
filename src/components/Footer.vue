3a950edda444e00c7cf8b5e5adcfef7767f446ec
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-05-27 17:29:42 -0300

    Navbar e Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,25 @@
 <template>
-<p>Footer</p>
+  <footer id="footer">
+    <p>Faça seu Hamburguer &copy; 2022</p>
+  </footer>
 </template>
 
 <script>
+
 export default {
-name:"Footer"
+  name:"Footer"
 }
 </script>
 
-<style>
-
+<style scoped>
+  #footer {
+    height: 150px;
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FCBA03;
+    width: 100%;
+  }
 </style>
